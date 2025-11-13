6bea95cff73cba5a9dfb79f85d6a74f9ae219bcd
izzy <izzy@dagsterlabs.com>
2025-11-13 00:40:46 -0700

    updated dag.csv.gz path to match multi-project structure

--- deep_purple_shared/src/deep_purple_shared/utils/constants.py
+++ deep_purple_shared/src/deep_purple_shared/utils/constants.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 DATETIME_FORMAT = "%Y-%m-%dT%H-%M-%S"
 ASSET_TYPE = "full_deep_purple_dummy_dag"
-DAG_CSV_PATH = Path(__file__).parent.parent.parent.parent.parent / "dag.csv.gz"
+DAG_CSV_PATH = Path(__file__).parent.parent.parent.parent / "dag.csv.gz"
